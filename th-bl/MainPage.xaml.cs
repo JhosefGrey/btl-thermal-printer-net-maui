3b51e5629aa9d8ea0c9bc7c73d6ea2feeaffafb1
jhosef <jreyes@kelpie.solutions>
2024-04-12 17:44:16 -0600

    cambios

--- th-bl/MainPage.xaml.cs
+++ th-bl/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Plugin.BLE;
 using Plugin.BLE.Abstractions;
 using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.iOS;
 
 namespace th_bl
 {
@@ -88,7 +89,68 @@ namespace th_bl
 
         private async Task OnPrint(IDevice device)
         {
+            try
+            {
 
+                var services = await device.GetServicesAsync();
+
+                var characteristics = await services.FirstOrDefault().GetCharacteristicsAsync();
+
+                //await _bluetoothAdapter.DisconnectDeviceAsync(device);
+                //var printer = new SerialPrinter(portName: device.Name, baudRate: 115200);
+
+                //var e = new EPSON();
+                //printer.Write( // or, if using and immediate printer, use await printer.WriteAsync
+                //  ByteSplicer.Combine(
+                //    e.CenterAlign(),
+                //    //e.PrintImage(File.ReadAllBytes("images/pd-logo-300.png"), true),
+                //    e.PrintLine(""),
+                //    e.SetBarcodeHeightInDots(360),
+                //    e.SetBarWidth(BarWidth.Default),
+                //    e.SetBarLabelPosition(BarLabelPrintPosition.None),
+                //    e.PrintBarcode(BarcodeType.ITF, "0123456789"),
+                //    e.PrintLine(""),
+                //    e.PrintLine("B&H PHOTO & VIDEO"),
+                //    e.PrintLine("420 NINTH AVE."),
+                //    e.PrintLine("NEW YORK, NY 10001"),
+                //    e.PrintLine("(212) 502-6380 - (800)947-9975"),
+                //    e.SetStyles(PrintStyle.Underline),
+                //    e.PrintLine("www.bhphotovideo.com"),
+                //    e.SetStyles(PrintStyle.None),
+                //    e.PrintLine(""),
+                //    e.LeftAlign(),
+                //    e.PrintLine("Order: 123456789        Date: 02/01/19"),
+                //    e.PrintLine(""),
+                //    e.PrintLine(""),
+                //    e.SetStyles(PrintStyle.FontB),
+                //    e.PrintLine("1   TRITON LOW-NOISE IN-LINE MICROPHONE PREAMP"),
+                //    e.PrintLine("    TRFETHEAD/FETHEAD                        89.95         89.95"),
+                //    e.PrintLine("----------------------------------------------------------------"),
+                //    e.RightAlign(),
+                //    e.PrintLine("SUBTOTAL         89.95"),
+                //    e.PrintLine("Total Order:         89.95"),
+                //    e.PrintLine("Total Payment:         89.95"),
+                //    e.PrintLine(""),
+                //    e.LeftAlign(),
+                //    e.SetStyles(PrintStyle.Bold | PrintStyle.FontB),
+                //    e.PrintLine("SOLD TO:                        SHIP TO:"),
+                //    e.SetStyles(PrintStyle.FontB),
+                //    e.PrintLine("  FIRSTN LASTNAME                 FIRSTN LASTNAME"),
+                //    e.PrintLine("  123 FAKE ST.                    123 FAKE ST."),
+                //    e.PrintLine("  DECATUR, IL 12345               DECATUR, IL 12345"),
+                //    e.PrintLine("  (123)456-7890                   (123)456-7890"),
+                //    e.PrintLine("  CUST: 87654321"),
+                //    e.PrintLine(""),
+                //    e.PrintLine("")
+                //  )
+                //);
+
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Error Imprimiendo", ex.Message, "Entendido");
+            }
         }
     }
 
--- th-bl/Platforms/Android/MainActivity.cs
+++ th-bl/Platforms/Android/MainActivity.cs
@@ -1,11 +1,36 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
 
 namespace th_bl
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Platform.Init(this, savedInstanceState);
+
+            await Permissions.RequestAsync<BLEPermission>();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        // Function below is required to ask for permission to use Bluetooth
+        public class BLEPermission : Permissions.BasePlatformPermission
+        {
+            public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
+            {
+                (Android.Manifest.Permission.BluetoothScan, true),
+                (Android.Manifest.Permission.BluetoothConnect, true)
+            }.ToArray();
+        }
     }
 }
